e3d8215b4349639f4380a3998dcd180f015357c5
rampara-genealogy <ramparagenealogy@gmail.com>
2024-06-17 14:50:58 +0530

    MSG

--- family/urls.py
+++ family/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', include("main.urls")),
 ]
 # add at the last
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if urlpatterns:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
