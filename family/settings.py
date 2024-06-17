b79e472b5c6ca6d3d33a07d7ff6f9555d1f35ee6
rampara-genealogy <ramparagenealogy@gmail.com>
2024-06-17 14:13:39 +0530

    MSG2

--- family/settings.py
+++ family/settings.py
@@ -153,9 +153,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'neelrajsinhzala27@gmail.com'
-# EMAIL_HOST_PASSWORD = 'syik suwx zkxf icga'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'neelrajsinhzala27@gmail.com'
+EMAIL_HOST_PASSWORD = 'syik suwx zkxf icga'
