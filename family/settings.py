6f44274145886987dc020df89e27cd6b5e657662
rampara-genealogy <ramparagenealogy@gmail.com>
2024-06-17 15:41:14 +0530

    MSG

--- family/settings.py
+++ family/settings.py
@@ -146,8 +146,8 @@ import os
 STATICFILES_DIRS = ['/Users/neel2004/Desktop/family/family/static',]
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
