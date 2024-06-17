cf5d079fbabfbb3306e45f20b75a856f3d04d114
rampara-genealogy <ramparagenealogy@gmail.com>
2024-06-17 15:06:43 +0530

    MSG

--- family/settings.py
+++ family/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'family.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-import os
+# import os
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -94,7 +94,17 @@ import os
 #         'PORT': os.getenv('PGPORT'),
 #     }
 # }
-DATABASES = {}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": os.environ.get("DB_NAME"),
+#         "USER": os.environ.get("DB_USER"),
+#         "PASSWORD": os.environ.get("DB_PASSWORD"),
+#         "HOST": os.environ.get("DB_HOST"),
+#         "PORT": os.environ.get("DB_PORT"),
+#         "SECRET_KEY": os.environ.get("SECRET_KEY"),
+#     }
+# }
 from dotenv import load_dotenv
 load_dotenv()
 
