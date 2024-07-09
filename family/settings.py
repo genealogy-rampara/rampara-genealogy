aee9adf3dffd1ecb8766d4e4864fcb92a36b65ca
rampara-genealogy <ramparagenealogy@gmail.com>
2024-07-09 14:17:47 +0530

    FINAL CODE

--- family/settings.py
+++ family/settings.py
@@ -75,13 +75,13 @@ WSGI_APPLICATION = 'family.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.dummy',
