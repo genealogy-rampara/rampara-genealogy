a3f89f92591b4434656f974d34ff9a8a6ebb92d9
rampara-genealogy <ramparagenealogy@gmail.com>
2024-07-17 15:27:27 +0530

    FINAL GENEALOGY CODE

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('import_data_from_csv/', views.import_data_from_csv, name='import_data_from_csv'),  
     path('generate_tree_data/', views.generate_tree_data, name='generate_tree_data'),
     path('search/', views.search_person, name='search_person'),
-    path('tree_with_female/', views.tree_with_female, name='tree_with_female'),
+    path('tree_including_daughter/', views.tree_with_female, name='tree_with_female'),
     path('login/', views.login, name='login'),
 ]
