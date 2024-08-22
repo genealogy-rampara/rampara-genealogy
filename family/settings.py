e0bcb42b684b139845bfe283010fed24c3878ae2
rampara-genealogy <ramparagenealogy@gmail.com>
2024-08-22 10:01:21 +0530

    UPDATED CODE

--- family/settings.py
+++ family/settings.py
@@ -146,3 +146,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ramparagenealogy@gmail.com'
+EMAIL_HOST_PASSWORD = 'shdn ggow cgqf dnby'
